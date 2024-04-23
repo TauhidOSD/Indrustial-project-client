efde389c220c1c49536bb93b1700dd3ab4e098a5
Tauhid hossen <tauhidhossen04@gmail.com>
2024-04-23 11:42:16 +0600

    User profile Update

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Currently, two official plugins are available:
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 # Indrusto 
-LiveUrl:(https://ornate-starlight-dbaa18.netlify.app/)
+LiveUrl:(https://clever-kheer-0e7947.netlify.app/)
 Comprehensive Industry Insights
 Interactive Forums and Communities
 Product Showcase and Reviews
--- src/Pages/Home/UpdateProfile/UpdateProfile.jsx
+++ src/Pages/Home/UpdateProfile/UpdateProfile.jsx
@@ -1,30 +1,29 @@
-import React, { useContext } from 'react';
-import { Helmet } from 'react-helmet-async';
-import { AuthContext } from '../../../FirebaseProvider/FirebaseProvider';
+import React, { useContext } from "react";
+import { Helmet } from "react-helmet-async";
+import { AuthContext } from "../../../FirebaseProvider/FirebaseProvider";
 
 const UpdateProfile = () => {
-  const {  updateUserData } = useContext(AuthContext);
-  const handleUpdate = (e)=>{
+  const { updateUserData } = useContext(AuthContext);
+  const handleUpdate = (e) => {
     console.log(e);
     e.preventDefault();
-    const name = e.target.name.value
-const photo = e.target.photo.value
-     
-    updateUserData (name,photo)
-    .then(()=>{
-      
-    })
-  }
-    return (
-        <div>
-            <Helmet>
-                <title>Indrusto || Update profile</title>
-            </Helmet>
-            <div className="hero-content">
+    const name = e.target.name.value;
+    const photo = e.target.photo.value;
+
+    updateUserData(name, photo).then(() => {});
+  };
+  return (
+    <div>
+      <Helmet>
+        <title>Indrusto || Update profile</title>
+      </Helmet>
+      <div className="hero-content">
         <div className="card w-full max-w-sm shadow-2xl bg-base-100">
           <div className="card-body">
-            <h1 className="text-3xl font-bold text-center mb-5">Update profile</h1>
-            <form onSubmit={handleUpdate} >
+            <h1 className="text-3xl font-bold text-center mb-5">
+              Update profile
+            </h1>
+            <form onSubmit={handleUpdate}>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Name</span>
@@ -49,30 +48,6 @@ const photo = e.target.photo.value
                   required
                 />
               </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="email"
-                  name="email"
-                  className="input input-bordered"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input
-                  type="password"
-                  placeholder="password"
-                  name="password"
-                  className="input input-bordered"
-                  required
-                />
-              </div>
 
               <div className="form-control mt-6">
                 <input
@@ -82,14 +57,12 @@ const photo = e.target.photo.value
                 />
               </div>
               <br />
-              
             </form>
-            
           </div>
         </div>
       </div>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default UpdateProfile;
